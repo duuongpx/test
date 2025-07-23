8c18c9533a08f0a014aa15a9d44625aaaf54b9fd
duuongpx <78335670+duuongpx@users.noreply.github.com>
2025-07-23 14:44:25 +0700

    Add simple menu script

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # test
+
+This repository contains a simple menu script.
+
+Run the script with:
+
+```
+python3 menu.py
+```
+
+Select `1` to print "xin chào" or `2` to print "kết thúc".
---
+++ menu.py
@@ -0,0 +1,10 @@
+print("Menu:")
+print("1. xin chào")
+print("2. kết thúc")
+choice = input("Chọn 1 hoặc 2: ")
+if choice == '1':
+    print("xin chào")
+elif choice == '2':
+    print("kết thúc")
+else:
+    print("Lựa chọn không hợp lệ")
